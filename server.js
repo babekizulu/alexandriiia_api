f119ef2890285746d8f6c6c8201cc6f3d0911a79
Lwandle Dlamini <babekizulu@gmail.com>
2025-04-07 13:12:45 +0200

    Added CORS Options

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ const visionRoute = require('./routes/vision');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://alexandriiia.com', 'http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use('/api/analyze', analysisRoute);
 app.use('/api/weave', weaveRoute);
